568f030fde9cfa9426b9359cecef298389ff7937
Carlos Abraao <71728180+CarlosAbraao@users.noreply.github.com>
2021-05-25 14:19:50 -0300

    Criado exemplo de entrada de dados
    Nesse exemplo uso a classe Scanner para criar um input

---
+++ Aula-Praticando-Java/src/basico/Aula_04_3_Entrada_Processamento_Saida_De_Dados.java
@@ -0,0 +1,38 @@
+package basico;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * OPERAÇÕES BÁSICAS DA PROGRAMÇÃO ENTRADA, PROCESSAMENTO E SAIDA
+ * 
+ * 					VAMOS VER COMO FUNCIONA O PROCESSAMENTO?
+ **/
+
+public class Aula_04_3_Entrada_Processamento_Saida_De_Dados {
+
+	public static void main(String[] args) {
+		
+	
+
+		
+		// A classe SCANNER PERMITE LER INFORMÇÕES DO TECLADO
+		/** NO EXEMPLO CRIEI UM OBJETO "TECLADO" QUE IRÁ CAPTURAR
+		
+		 * INFORMÇÕES DO TECLADO FISICO, VEJA O EXEMPLO
+		
+		 * **/
+		Scanner teclado = new Scanner(System.in);
+
+		// A VARIAVEL VALOR (DO TIPO DOUBLE) ESTA RECEBENDO UMA ENTRADA DO TECLADO
+		double valor = teclado.nextDouble();
+
+		System.out.println(valor);
+
+		// EX: 1 USANDO PRINT FORMATADO; AQUI VOCÊ PODE ESTABELER O NUMERO
+		// DE CASA DECIMAIS
+		System.out.printf("%.5f%n", valor);
+
+	}
+
+}
