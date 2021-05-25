31bf1e33fc86294211cc0895f518f0c972c7333b
Carlos Abraao <71728180+CarlosAbraao@users.noreply.github.com>
2021-05-25 13:07:07 -0300

    Criada a Aula_03
    Nesse exemplo mostro como usar variáveis

---
+++ Aula-Praticando-Java/src/basico/Aula_03_Variaveis.java
@@ -0,0 +1,40 @@
+package basico;
+
+public class Aula_03_Variaveis {
+
+	public static void main(String[] args) {
+		/**
+		 * VARIAVEIS SÃO LOCAIS DE ARMAZENAMENTO DE DADOS
+		 * VARIAVEIS PRECISAM SER DECLARADAS
+		 * EX DE USO: <TIPO> <NOME> = <VALOR DA VARIAVEL>;
+		 * 
+		 * ALGUNS TIPOS DE VARIAVEIS
+		 * 
+		 * 
+		 * byte
+		 * short
+		 * int
+		 * long
+		 * double
+		 * float
+		 * char
+		 * boolean
+		 * String (essa variavel representa uma cadeira de caracteres)	
+	
+		 * **/
+		
+		//EXEMPLO USO PRÁTICO
+		
+		int idade = 35;
+		double valor = 15.50;
+		String nome = "Carlos Abraão";
+		
+		//IMPRIMINDO A VARIAVEL
+		
+		System.out.println(nome);
+		System.out.println(idade);
+		System.out.println(valor);
+
+	}
+
+}
