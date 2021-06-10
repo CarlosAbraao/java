93e00d9623c8c7accd0d0891331dc8bb5d2d83fc
Carlos Abraao <71728180+CarlosAbraao@users.noreply.github.com>
2021-06-10 14:22:07 -0300

    Criado usando casting

---
+++ Aula-Praticando-Java/src/basico/Aula_05_Usando_Casting.java
@@ -0,0 +1,23 @@
+package basico;
+
+public class Aula_05_Usando_Casting {
+
+	public static void main(String[] args) {
+
+// EXEMPLO DE USO DO CASTING
+		
+		int a = 5;
+		int b = 2;
+		
+		// USANDO O CASTING
+		
+		//double resultado = 5 / 2;
+		double resultado =(double) 5 / 2;
+		
+		// 2 VALORE INTEIROS SENDO DIVIDIDOS UM PELO OUTRO O RESULTADO SEM O CASTING É 2.
+		
+		System.out.println(resultado);
+
+	}
+
+}
