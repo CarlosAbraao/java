e15d7e45b2ab7d62804a5955243e472e700f45a1
Carlos Abraao <71728180+CarlosAbraao@users.noreply.github.com>
2021-08-29 17:22:48 -0300

    Primeiro2908_inicio

---
+++ TreinamentoNelioJava/Aula_01_HelloWord/src/HelloWord.java
@@ -0,0 +1,27 @@
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// Olá Mundo em JAVA
+		
+		System.out.println("Hello World");
+		
+		System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+		
+		System.out.println(hello());
+		
+		
+
+	}
+	
+	// CRIEI UM METODO QUE RETORNA UMA STRING
+	public static String hello() {
+		
+		return "Olá Mundo";
+	}
+
+}
+
+
+
+
