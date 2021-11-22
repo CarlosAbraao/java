e543e54195fcbd04d698a214d78c96fdf94486f9
Carlos Abraão <carloschavesnd@hotmail.com>
2021-11-22 11:11:23 -0300

    Inicio do projeto
    criei algumas pastas para organizar o projeto

---
+++ SpringBoot/projeto-usuario_api/projeto-usuario_api/src/main/java/com/devcarlos_api/ProjetoUsuarioApiApplication.java
@@ -0,0 +1,13 @@
+package com.devcarlos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoUsuarioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoUsuarioApiApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/projeto-usuario_api/projeto-usuario_api/src/test/java/com/devcarlos_api/ProjetoUsuarioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcarlos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoUsuarioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
